ced5af56bd17d4c5a76369cbe5ec89879b00a8bb
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-23 15:29:30 +0300

    [163415548] added the __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,4 @@
-from flask import Flask
-from app.api.v2.database.database import initialize
-from app.api.v2.views.user_views import app_blueprint
-from app.api.v2.utils.hanlders import method_not_allowed, not_found, bad_request, access_forbiden, server_error
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(app_blueprint)
-    app.register_error_handler(405, method_not_allowed)
-    app.register_error_handler(400, bad_request)
-    app.register_error_handler(500, server_error)
-    app.register_error_handler(404, not_found)
-    app.register_error_handler(403, access_forbiden)
-
-    initialize()
-    return app
+"""contains the configurations for the application"""
+class Config:
+    Debug = True
+    
