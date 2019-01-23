2b4a3e334b52b3192d0236ef1fcbb368520d0d98
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-23 03:54:48 +0300

    [finish#163249431] added the user view for sign up

--- app/api/v2/views/user_views.py
+++ app/api/v2/views/user_views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash
+from app.api.v2.utils.validation import Validators
+from app.api.v2.models.user_models import UserModel
+from app.api import app_blueprint
 
-user_blueprint= Blueprint("user_blueprint", __name__, url_prefix="/api/v2/auth/")
-
-
-@user_blueprint.route("/signup",methods=["POST"])
+validators = Validators()   
+user_model = UserModel()
+@app_blueprint.route("/signup",methods=["POST"])
 def create_user():
     data = request.json
     required_fields = ("username", "password", "email", "firstname", "lastname", "phonenumber")
@@ -21,3 +24,61 @@ def create_user():
                               ],
                      "status" : 400
                     }), 400
+
+    username = data.get("username")
+    password = data.get("password")
+    email = data.get("email")
+    firstname = data.get("email")
+    lastname = data.get("lastname")
+    phonenumber = data.get("phonenumber")
+    isadmin = data.get("isadmin")
+
+    hashed_pass= generate_password_hash(password)
+    user_data = {
+                    "username" : username,
+                    "password" : hashed_pass,
+                    "email" : email,
+                    "firstname" : firstname,
+                    "lastname" : lastname,
+                    "phonenumber" : phonenumber,
+                    "isdamin": isadmin
+                }
+
+    if user_model.check_for_same_email(user_data) == False:
+        return jsonify({"error": "the email already exists",
+                         "status":409}),409
+
+    if user_model.check_for_same_username(user_data) == False:
+        return jsonify({"error": "the same username already exists", 
+                        "status":409}
+                    ),409
+
+
+    to_be_string = (username, password, email, firstname, lastname)
+
+    
+    if validators.check_if_string_(to_be_string) == False:
+        return jsonify({"status" : 400, 
+                        "error":[{"username": " need to be a string",
+                                  "email" : "needs to be a string",
+                                  "firstname" : "needs to be string", 
+                                  "lastname" :  "needs to be a string", 
+                                  "password" : " needs to be a string"
+                                }]}),400
+
+    if validators.check_if_valid_email (user_data)== False:
+        return jsonify(
+                        {"status":400,
+                         "error" : "invalid email address"
+                        }
+                      ), 400
+                    
+    if validators.check_if_valid_phone_number(user_data) ==  False:
+        return jsonify({
+                        "status" : 400,
+                        "error" : "phone number should be in the format 020 254 2542"
+        }),400
+
+   
+    user_model_response = user_model.save(user_data)
+    return jsonify(user_model_response),201
