d48a95834f18003127e85fad3c88e4fe92e6c7a1
teekize <elvisteeka1@gmail.com>
2019-03-18 15:27:37 +0300

    added the upvote method , check_if blacklisted and blacklist methods

--- app/api/v2/models/question_models.py
+++ app/api/v2/models/question_models.py
@@ -1,5 +1,7 @@
 from app.api.v2.database.database import DbModels
-from app.api.v2.database.sql_queries import save_question,get_meetup_by_id
+from app.api.v2.database.sql_queries import (save_question,get_meetup_by_id, 
+                                            get_question_by_id, get_all_questions, update_votes, insert_blacklisted,
+                                            check_if_blacklisted)
 import datetime
 from psycopg2 import IntegrityError
 from psycopg2.extras import RealDictCursor
@@ -7,11 +9,20 @@ from psycopg2.extras import RealDictCursor
 """this file conatins the questions methods that are called by the view"""
 class QuestionModel(DbModels):
     """class containing the questionmodel methods"""
+    
     def __init__(self):
         self.day_created = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M')
 
     "question_id | createdon | createdby | meetup | title | body | votes"
+    def get_all_questions(self):
+        conn = self.db_connection()
+        cur =conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(get_all_questions,)
+        results = cur.fetchmany()
+        conn.commit()
+        conn.close()
 
+        return results
 
     def save_question(self, data):
         try:
@@ -26,6 +37,8 @@ class QuestionModel(DbModels):
             cur = conn.cursor(cursor_factory=RealDictCursor)
             cur.execute(save_question,(data_to_pass))
             results = cur.fetchone()
+            conn.commit()
+            conn.close()
 
             return {
                     "status":201,
@@ -42,7 +55,7 @@ class QuestionModel(DbModels):
             conn = self.db_connection()
             cur = conn.cursor()
             cur.execute(get_meetup_by_id, (meetup_id,))
-            results = cur.fetchone()
+            # results = cur.fetchone()
             conn.commit()
             conn.close()
 
@@ -52,3 +65,69 @@ class QuestionModel(DbModels):
                     "error":"could not find meetup with that id",
                      "status":404
                      }
+
+    def check_if_question_exixts(self, data, username):
+        """this function checks if the question exists in the database
+        takes in the parameter data which is the qustion id 
+        it then returns the false is the results variable is none and 
+        true is its found
+        """
+        # person = username[1]
+        conn = self.db_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(get_question_by_id, (data,))
+        results = cur.fetchone()
+        conn.commit()
+        conn.close()
+        
+
+        if not results:
+            return False
+        else :
+            return results
+
+    def upvote_question(self, question_id, username):
+        user_id = username[1]
+        print (user_id)
+
+        question = self.check_if_question_exixts(question_id, username)
+        blacklisted = self.check_if_user_and_question_blacklisted(user_id, question_id)
+
+
+        if blacklisted:
+            return {"message": "you cannot upvote twice", "status":403}
+
+        new_vote = question["votes"]+1
+        conn = self.db_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(update_votes, (new_vote, question_id))
+        results = cur.fetchone()
+        conn.commit()
+        conn.close()
+
+        results["message"]="you have upvoted the question"
+        
+
+        """after the upvote we then added the user_id and question_id into the blacklisted_users and questions"""
+        self.blacklisted_users(user_id, question_id)
+        return (results)
+
+    def blacklisted_users(self, user_id, question_id):
+        conn = self.db_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(insert_blacklisted, (user_id, question_id))
+        results = cur.fetchone()
+        conn.commit()
+        conn.close()
+
+        return results
+
+    def check_if_user_and_question_blacklisted(self, user_id, question_id):
+        conn = self.db_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(check_if_blacklisted, (user_id, question_id))
+        results = cur.fetchone()
+        conn.commit()
+        conn.close()
+
+        return results
